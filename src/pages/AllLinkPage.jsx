5b8a5329e2bf55126fb6a4d33bf332e9ac165298
Aniket Mahajan <mahajananiket207@gmail.com>
2025-03-21 15:17:05 +0530

    add all link page

--- src/pages/AllLinkPage.jsx
+++ src/pages/AllLinkPage.jsx
@@ -6,8 +6,12 @@ export default function AllLinkPage(){
 
   return (
     <div className="h-screen w-screen flex justify-center items-center bg-gray-100">
-        <div className="w-full max-w-4xl h-[80vh] overflow-auto bg-white shadow-lg rounded-lg p-4">
+        <div className="w-full max-w-4xl h-[80vh] overflow-auto bg-white rounded-lg p-4">
           <h2 className="text-xl font-semibold text-gray-700 mg-4 text-center">Shortened links</h2>
+          <div className="grid grid-cols-2 mt-2 font-bold bg-gray-200 p-2 rounded-md">
+            <div>Links</div>
+            <div>Created At</div>
+          </div>
         </div>
     </div>
 
